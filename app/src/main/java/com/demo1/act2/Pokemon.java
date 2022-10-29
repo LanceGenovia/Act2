d26dc2182896a1d78b9665df45421609053950a4
LanceGenovia <113650745+LanceGenovia@users.noreply.github.com>
2022-10-29 13:41:05 +0800

    Initial commit

---
+++ app/src/main/java/com/demo1/act2/Pokemon.java
@@ -0,0 +1,23 @@
+package com.demo1.act2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pokemon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheesh);
+
+    }
+}
